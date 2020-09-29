8c9ff435cf57e9e6a07299f416af99570042d658
Gabriel LaBrie <gabriel.labrie.professional@gmail.com>
2020-09-28 19:16:47 -0600

    removed syntax errors in productQuestionMap

--- client/src/modules/productQuestionMap.jsx
+++ client/src/modules/productQuestionMap.jsx
@@ -2,18 +2,16 @@ import React from 'react';
 import ProductQuestionForm from './productQuestion.jsx';
 
 const ProductQuestionMap = (props) => {
-  console.log('props in mapping', props.question);
   // eslint-disable-next-line space-before-blocks
-  if(props.question[0] !== undefined){
+  if (props.question[0] !== undefined) {
     const mapProcuctQuestion = props.question;
-     //console.log('in cond', mapProcuctQuestion);
-    return(
+    return (
       <div className="questionsMap">
         {mapProcuctQuestion.map((questionObject) =>
           <ProductQuestionForm productQuestion={questionObject} />)}
       </div>
-    )
+    );
   }
-return <div>loading</div>;
+  return <div>loading</div>;
 };
 export default ProductQuestionMap;
