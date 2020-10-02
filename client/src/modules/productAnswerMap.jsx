f67fb067a94f6dd6bea0af7437091551365cad67
Gabriel LaBrie <gabriel.labrie.professional@gmail.com>
2020-10-01 18:55:24 -0600

    added toggle button to see more than two questions and back to two questions

--- client/src/modules/productAnswerMap.jsx
+++ client/src/modules/productAnswerMap.jsx
@@ -1,17 +1,24 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable import/no-cycle */
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable react/jsx-first-prop-new-line */
+/* eslint-disable react/jsx-max-props-per-line */
 /* eslint-disable import/extensions */
-import React, { useState, useEffect } from 'react';
-import getProductAnswerGet from '../api/getproductanswer';
- import ProductAnswer from './productAnswer.jsx';
-console.log('inside pam');
 
-const ProductAnswerMap = ( { newQuestion } ) => {
+import React, { useState, useEffect } from 'react';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+import getProductAnswerGet from '../api/getproductanswer';
+import ProductAnswer from './productAnswer.jsx';
+import GlobalStyle from '../components/globalStyle';
+
+const ProductAnswerMap = ({ newQuestion }) => {
   const questionId = newQuestion.question_id;
   const [answerId, setAnswerId] = useState([questionId]);
 
   useEffect(() => {
     getProductAnswerGet(questionId)
       .then((res) => {
-        console.log('res', res.data.results);
         setAnswerId(res.data.results);
       })
       .catch((err) => {
@@ -19,9 +26,29 @@ const ProductAnswerMap = ( { newQuestion } ) => {
       });
   }, [questionId]);
 
+  const mappedProductAnswers = answerId.map((answerArray, i) => (
+    <ProductAnswer answer={answerArray} keys={i}/>));
+  const [boolean, setBoolean] = useState(true);
+
   return (
     <div className="mapProductAnswer">
-      {answerId.map((answerObject) => <ProductAnswer answer={answerObject} />)}
+      <GlobalStyle />
+      {mappedProductAnswers.slice(0, 2)}
+      {mappedProductAnswers.length > 2 && (
+      <Accordion>
+        <Accordion.Collapse eventKey="0">
+          <div>
+            {mappedProductAnswers.slice(2)}
+          </div>
+        </Accordion.Collapse>
+        <Accordion.Toggle className="button" as={Card.Header} varient="link" eventKey="0"
+          onClick={() => setBoolean(!boolean)}
+        >
+          {boolean && 'Load More Answers'} {!boolean && 'See Less Answers'}
+        </Accordion.Toggle>
+      </Accordion>
+      )}
+
     </div>
   );
 };
--- client/src/modules/productQuestionMap.jsx
+++ client/src/modules/productQuestionMap.jsx
@@ -1,7 +1,13 @@
+/* eslint-disable import/extensions */
+/* eslint-disable max-len */
+/* eslint-disable semi */
+
 import React, { useState, useEffect } from 'react';
-// eslint-disable-next-line import/extensions
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
 import ProductQuestion from './productQuestion.jsx';
 import getProductQuestionGet from '../api/getproductquestion';
+import GlobalStyle from '../components/globalStyle';
 
 const ProductQuestionMap = () => {
   const [productId] = useState(10);// this will be whatever product Id is being viewed
@@ -17,10 +23,28 @@ const ProductQuestionMap = () => {
       });
   }, [productId]);
 
+  const mappedProductQuestion = productQuestionObject.map((questionObject, i) => <ProductQuestion productQuestion={questionObject} keys={i} />)
+  const [boolean, setBoolean] = useState(true);
+
   return (
-    <div>
-      {/* eslint-disable-next-line max-len */}
-      {productQuestionObject.map((questionObject) => <ProductQuestion productQuestion={questionObject} />)}
+    <div className="mapProductAnswer">
+      <GlobalStyle />
+      {mappedProductQuestion.slice(0, 2)}
+      {mappedProductQuestion.length > 2 && (
+      <Accordion>
+        <Accordion.Collapse eventKey="0">
+          <div>
+            {mappedProductQuestion.slice(2)}
+          </div>
+        </Accordion.Collapse>
+        <Accordion.Toggle className="button" as={Card.Header} varient="link" eventKey="0"
+          onClick={() => setBoolean(!boolean)}
+        >
+          {boolean && 'Load More Answers'} {!boolean && 'See Less Answers'}
+        </Accordion.Toggle>
+      </Accordion>
+      )}
+
     </div>
   );
 };
