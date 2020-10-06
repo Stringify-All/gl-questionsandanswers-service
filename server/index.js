986eff817f2d6c95c310c3e48e2ad1d441751d2f
Gabriel LaBrie <gabriel.labrie.professional@gmail.com>
2020-10-05 19:44:53 -0600

    Optimized for proxy

--- server/index.js
+++ server/index.js
@@ -2,18 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 9002;
-const getApiData = require('./api-helper.js');
-const ServerStyleSheet = require('styled-components');
-const cors = require('cors');
-
-app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
-app.use(express.static('/../client/dist'));
-
+app.use(express.static('../client/dist'));
 // Requests here
-
 app.listen(PORT, () => {
   console.log(`Hello, Scrumdog.  Your server is running on PORT: ${PORT}`);
 });
